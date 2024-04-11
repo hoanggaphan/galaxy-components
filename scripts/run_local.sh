6d7a9b574ad3c3938b4f3fdf9801f1c88242c483
hoanggaphan <dev.phansihoang@gmail.com>
2024-04-11 12:05:13 +0700

    update config

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -7,19 +7,65 @@ source .env.development
 response=$(npx retool-ccl login --status)
 
 # If already logged in, run `npx retool-ccl dev`
-if [[ $response == *"Currently logged in to https://$SUB_DOMAIN.retool.com."* ]]; then
-    npx retool-ccl dev
-else
-    # If not logged in, perform the login process
+if [[ $response == *"Currently logged in"* ]]; then
     {
+        sleep 1;
+        echo "Y"; # Enter to proceed
+        sleep 1;
         echo ""; # Enter to proceed
         sleep 1;
-        echo "${{secrets.SUB_DOMAIN}}"; # Enter subdomain from secrets
+        echo $RETOOL_SUB_DOMAIN; # Enter subdomain from secrets
         sleep 1;
-        echo "${{secrets.RETOOL_CUSTOM_COMPONENT_TOKEN}}"; # Enter token from secrets
+        echo $RETOOL_CUSTOM_COMPONENT_TOKEN; # Enter token from secrets
         sleep 1;
     } | npx retool-ccl login # Use the login command with entered information
 
-    # After successful login, run `npx retool-ccl dev`
-    npx retool-ccl dev 
+    # After successful login 
+    # run `npx retool-ccl dev`
+    response_dev=$(npx retool-ccl dev)
+
+    # Check if the library does not exist on the backend
+    if [[ $response_dev == *"Error, library does not exist on the backend"* ]]; then
+        {
+            sleep 1;
+            echo $RETOOL_CUSTOM_COMPONENT_LABEL; # Enter to proceed
+            sleep 1;
+            echo $RETOOL_CUSTOM_COMPONENT_DESCRIPTION; # Enter to proceed
+            sleep 1;
+        } | npx retool-ccl init
+
+        # After initialization, run `npx retool-ccl dev` again
+        npx retool-ccl dev
+    fi
+
+else
+    # If not logged in, perform the login process
+    {
+        sleep 1;
+        echo ""; # Enter to proceed
+        sleep 1;
+        echo $RETOOL_SUB_DOMAIN; # Enter subdomain from secrets
+        sleep 1;
+        echo $RETOOL_CUSTOM_COMPONENT_TOKEN; # Enter token from secrets
+        sleep 1;
+    } | npx retool-ccl login # Use the login command with entered information
+
+    # After successful login 
+    # run `npx retool-ccl dev`
+    response_dev=$(npx retool-ccl dev)
+
+    # Check if the library does not exist on the backend
+    if [[ $response_dev == *"Error, library does not exist on the backend"* ]]; then
+        {
+            sleep 1;
+            echo $RETOOL_CUSTOM_COMPONENT_LABEL; # Enter to proceed
+            sleep 1;
+            echo $RETOOL_CUSTOM_COMPONENT_DESCRIPTION; # Enter to proceed
+            sleep 1;
+        } | npx retool-ccl init
+        
+        # After initialization, run `npx retool-ccl dev` again
+        npx retool-ccl dev
+    fi
+
 fi
